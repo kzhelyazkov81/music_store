0168cb4a080a5f0aeea8a9f45247ad984ec47ce3
Kolyo Zhelyazkov <kzhelyazkov81@gmail.com>
2022-12-15 15:19:18 +0200

    update

---
+++ tests/accounts/views/sign_up_view_tests.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+UserModel = get_user_model()
+
+
+class SignUpViewTest(TestCase):
+    def test_sign_up__when_anonymous_user__expect_create_account(self):
+        credentials = {
+            'username': 'username',
+            'email': 'some@mail.com',
+            'password': '!zaq2wsx',
+        }
+
+        UserModel.objects.create_user(**credentials)
+        self.client.login(**credentials)
+
+        response = self.client.post(
+            reverse('create'),
+            data=credentials,
+        )
+
+        self.assertEqual(credentials['username'], response.context['user'].username)
