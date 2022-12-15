38afc73836d18c5784dc1c0a7f07efb9f15cce70
Kolyo Zhelyazkov <kzhelyazkov81@gmail.com>
2022-12-15 08:54:34 +0200

    update

--- tests/accounts/views/account_edit_view_tests.py
+++ tests/accounts/views/account_edit_view_tests.py
@@ -37,6 +37,7 @@ class AccountEditViewTest(TestCase):
 
         credentials = {
             'username': 'username',
+            'email': 'some@mail.com',
             'password': '!zaq2wsx',
         }
 
